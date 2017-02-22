000a443429ead693ac0c28c279b6e52be5605504
Diego Rodriguez <dxanderg@gmail.com>
2017-02-21 21:59:27 -0500

    Form Consultas Inicio

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Script para añadir registros al formulario de traslados
 //-----------------------------------------------------------------------------
 $(document).ready(function(){
-  var i=1;
+  var i=2;
   dibujar(i)
   $("#add_row").click(function(){
     $('#addr'+i).html("<td>"+ (i+1) +"</td><td><div id='select-list'><div class=''><select class='form-control'><option></option></select></div></div></td><td><div id='select-list'><div class=''><select class='form-control'><option></option></select></div></div></td><td><div id='select-list'><div class=''><select id='list-activosi" + (i+1) +"' multiple='multiple'><option value='1'>CPU</option><option value='2'>Monitor</option><option value='3'>Hardphone</option></select></div></div></td><td><i class='fa fa-arrows-h'></i></td><td><div id='select-list'><div class=''><select class='form-control'><option></option></select></div></div></td><td><div id='select-list'><div class=''><select class='form-control'><option></option></select></div></div></td><td><div id='select-list'><div class=''><select id='list-activosf" + (i+1) +"' multiple='multiple'><option value='1'>CPU</option><option value='2'>Monitor</option><option value='3'>Hardphone</option></select></div></div></td><td><input type='checkbox' name='inversa' class='form-check-input big-checkbox'></td></tr>");
@@ -21,16 +21,30 @@ $(document).ready(function(){
 //Scrip de Dropdown Seleccion multiple.
 //-----------------------------------------------------------------------------
 function dibujar(i){
-  $('#list-activosi'+i).multiselect({
-    includeSelectAllOption: true
-  });
-  $('#list-activosf'+i).multiselect({
-    includeSelectAllOption: true
-  });
+  if (i==2){
+    $('#list-activosi1').multiselect({
+      includeSelectAllOption: true
+    });
+    $('#list-activosf1').multiselect({
+      includeSelectAllOption: true
+    });
+    $('#list-activosi2').multiselect({
+      includeSelectAllOption: true
+    });
+    $('#list-activosf2').multiselect({
+      includeSelectAllOption: true
+    });
+  }
+  else{
+    $('#list-activosi'+i).multiselect({
+      includeSelectAllOption: true
+    });
+    $('#list-activosf'+i).multiselect({
+      includeSelectAllOption: true
+    });
+  }
 }
 //-----------------------------------------------------------------------------
-
-
 
 
 //Scrip de Dropdown Seleccion multiple.
